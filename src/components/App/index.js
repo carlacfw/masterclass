6f73f023fa5084ee75966c10d92f3d09b6e2d26c
Carla Weiss <carlacfw@gmail.com>
2018-05-26 09:02:44 +1200

    tests error fixed on setupTests.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,26 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { times } from 'ramda'
-import { isUndefined } from 'ramda-adjunct'
 
 import { Board, Square } from '..'
-import { getPlayer } from '../../utilities'
-
-function makeSquares (moves) {
-  return times(square => {
-    const player = getPlayer(square, moves)
-
-    return isUndefined(player) ? (
-      <Square
-        key={square}
-        index={square}
-        handleClick={() => console.log(`Square ${square}`)}
-      />
-    ) : (
-      <Square key={square} index={square} player={player} />
-    )
-  }, 9)
-}
 
 const StyledApp = styled.div`
   display: grid;
@@ -32,10 +14,12 @@ const StyledApp = styled.div`
   width: 100vw;
 `
 
-export default function App ({ moves = [] }) {
+export default function App () {
   return (
     <StyledApp>
-      <Board>{makeSquares(moves)}</Board>
+      <Board>
+        {times(square => <Square key={square} index={square} />, 9)}
+      </Board>
     </StyledApp>
   )
 }
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,7 @@ import Adapter from 'enzyme-adapter-react-16'
 import 'jest-enzyme'
 import 'jest-styled-components'
 import toJson from 'enzyme-to-json'
-
+/*eslint-disable */
 Enzyme.configure({ adapter: new Adapter() })
-
+/*eslint-disable */
 global.toJson = toJson
